52d323a231a3b18b01743a1387662a74c82d4b69
msorenso <130500354+msorenso@users.noreply.github.com>
2024-10-15 17:12:12 -0400

    Update pruning-jim.py

--- Code Files/pruning-jim.py
+++ Code Files/pruning-jim.py
@@ -2,6 +2,7 @@
 
 from typing import List, Generator, Optional
 import random
+import sys
 
 #pruning
 def gen_subsets(jar: List[int], subset: List[Optional[int]], ind: int, target: int) -> Generator:
@@ -18,18 +19,19 @@ def gen_subsets(jar: List[int], subset: List[Optional[int]], ind: int, target: i
         yield from gen_subsets(jar, subset + [jar[ind]], ind+1, target)
 
 def main():
-    jar = [
-    5, 10, 1, 25, 5, 10, 1, 1, 25, 10,
-    5, 25, 10, 5, 1, 25, 1, 10, 5, 25,
-    10, 1, 5, 25, 10, 1, 5, 10, 25, 1,
-    5, 10, 25, 5, 1, 10, 25, 1, 5, 10,
-    25, 1, 25, 10, 5, 1, 25, 10, 5, 1
-    ]
+    target = int(sys.stdin.readline().rstrip())
+    jar = [int(n) for n in sys.stdin.readline().rstrip().split(', ')]
     min = jar
-    for result in gen_subsets(jar, [], 0, 27):
-        if len(result) < len(min):
-            min = result
-    print(min)
+    print(f'Target: {target}')
+    gen = gen_subsets(jar, [], 0, target)
+    
+    if not gen:
+        print('No possible combination.')
+    else:
+        for result in gen:
+            if len(result) < len(min):
+                min = result
+        print(min)
     
 if __name__ == '__main__':
     main()
