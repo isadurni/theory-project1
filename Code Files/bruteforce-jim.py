35fc02e91455748c77c10e51b90215450b781ef0
msorenso <130500354+msorenso@users.noreply.github.com>
2024-10-15 17:11:29 -0400

    Update bruteforce-jim.py

--- Code Files/bruteforce-jim.py
+++ Code Files/bruteforce-jim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import List, Generator, Optional
-
+import sys
 
 def gen_subsets(jar: List[int], subset: List[Optional[int]], ind: int, target: int) -> Generator:
     if ind == len(jar):
@@ -14,12 +14,19 @@ def gen_subsets(jar: List[int], subset: List[Optional[int]], ind: int, target: i
         yield from gen_subsets(jar, subset + [jar[ind]], ind+1, target)
 
 def main():
-    jar = ([1] * 5) + ([5] * 3) + ([10] * 4) + ([25] * 2)
+    target = int(sys.stdin.readline().rstrip())
+    jar = [int(n) for n in sys.stdin.readline().rstrip().split(', ')]
     min = jar
-    for result in gen_subsets(jar, [], 0, 27):
-        if len(result) < len(min):
-            min = result
-    print(min)
+    print(f'Target: {target}')
+    gen = gen_subsets(jar, [], 0, target)
+    
+    if not gen:
+        print('No possible combination.')
+    else:
+        for result in gen:
+            if len(result) < len(min):
+                min = result
+        print(min)
     
 if __name__ == '__main__':
     main()
